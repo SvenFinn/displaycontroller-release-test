4ce8777db296790ff4949dfc5e27c8cd7f3f1b5a
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2024-08-08 08:32:52 +0200

    feat: Seeding

--- src/database/prisma/src/entrypoint.sh
+++ src/database/prisma/src/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ls -al
+source .env
+
+DB_HOST="$(echo $SELF_DB_URL | cut -d'@' -f2 | cut -d':' -f1)"
+
+until nc -z -v -w30 "$DB_HOST" 5432
+do
+    sleep 1
+done
 
 npx prisma migrate deploy
-npx prisma db seed
+
+node ./seed.js
--- src/database/prisma/src/seed.ts
+++ src/database/prisma/src/seed.ts
@@ -1,23 +1,38 @@
 import { PrismaClient } from '@prisma/client';
+import pino from 'pino';
+
+const logger = pino({
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+        },
+    },
+});
 
 const prisma = new PrismaClient();
 
 async function main() {
     const count = await prisma.parameter.count();
     if (count > 0) {
-        console.log("Parameters already seeded, skipping...");
+        logger.info("Database already seeded, skipping");
         return;
     }
+    logger.info("Seeding parameters");
     await prisma.parameter.createMany({
         data: [
             { name: "MEYTON_SERVER_IP", strValue: "192.168.10.200" },
             { name: "ENABLE_TIME_SYNC", boolValue: true },
+            { name: "EVALUATION_SYNC_INTERVAL", numValue: 60000 },
         ]
     });
+    logger.info("Seeding complete");
     await prisma.$disconnect();
 }
 
 main().catch(e => {
+    logger.error(e);
     throw e;
 }).finally(async () => {
     await prisma.$disconnect();
