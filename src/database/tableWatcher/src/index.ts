d5ef99f9f513d4e17499390409fd2c17a9ecd4b0
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2024-10-01 13:10:52 +0200

    feat: TableWatcher eventsource

--- src/database/tableWatcher/src/index.ts
+++ src/database/tableWatcher/src/index.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 import { EventEmitter } from 'events';
+import EventSource from "eventsource";
+import pino from "pino";
+
+const logger = pino({
+    level: "info",
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+        },
+    },
+});
 
 export class TableWatcher extends EventEmitter {
     private prisma: PrismaClient;
     private tables: string[];
     private interval: number;
-    private checksums: Record<string, number> = {};
+    private checksums: Record<string, BigInt> = {};
     private nodeInterval: NodeJS.Timeout | null = null;
     private serverState: boolean = false;
+    private running: boolean = false;
 
     constructor(prisma: PrismaClient, tables: string[], interval: number = 10000) {
         super();
@@ -15,35 +28,70 @@ export class TableWatcher extends EventEmitter {
         this.tables = tables;
         this.interval = interval;
         for (let i = 0; i < this.tables.length; i++) {
-            this.checksums[this.tables[i]] = 0;
+            this.checksums[this.tables[i]] = 0n;
         }
-
+        const events = new EventSource("http://check-server:80/api/serverState/sse");
+        events.onopen = () => {
+            logger.info("Connected to server state SSE");
+        }
+        events.onmessage = async (event: MessageEvent) => {
+            logger.info("Received server state");
+            const curState = JSON.parse(event.data);
+            if (curState != this.serverState) {
+                this.serverState = curState;
+                if (this.serverState) {
+                    await this.startInternal();
+                } else {
+                    await this.stopInternal();
+                }
+            }
+        }
     }
 
-    public async start() {
-        this.stop();
-        this.nodeInterval = setInterval(() => this.watch(), this.interval);
+    private async startInternal() {
+        this.stopInternal();
+        if (this.running) {
+            this.nodeInterval = setInterval(() => this.watch(), this.interval);
+            await this.watch();
+        }
     }
 
-    public async stop() {
+    public async stopInternal() {
         if (this.nodeInterval !== null) {
             clearInterval(this.nodeInterval);
         }
     }
 
+    public async start() {
+        this.running = true;
+        if (this.serverState) {
+            this.startInternal();
+        }
+    }
+
+    public async stop() {
+        this.running = false;
+        this.stopInternal();
+    }
+
     public async setInterval(interval: number) {
         this.interval = interval;
-        this.start();
+        this.startInternal();
     }
 
     private async watch() {
+        logger.debug("Checking table checksums");
         let changed: string[] = [];
         await Promise.all(this.tables.map(async (table) => {
             try {
-                let checksum = await this.prisma.$queryRawUnsafe(`CHECKSUM TABLE ${table}`) as { Checksum: number };
-                if (checksum.Checksum !== this.checksums[table]) {
+                let checksum = await this.prisma.$queryRawUnsafe(`CHECKSUM TABLE ${table}`) as [{ Table: string, Checksum: BigInt }];
+                if (checksum.length != 1) {
+                    throw new Error("Invalid checksum response");
+                }
+                const checksumValue = checksum[0].Checksum;
+                if (checksumValue !== this.checksums[table]) {
                     changed.push(table);
-                    this.checksums[table] = checksum.Checksum;
+                    this.checksums[table] = checksumValue;
                 }
             } catch (error) {
                 console.error(error);
@@ -53,5 +101,9 @@ export class TableWatcher extends EventEmitter {
         if (changed.length > 0) {
             this.emit('change', changed);
         }
+    }
+
+    public on(event: 'change', listener: (tables: string[]) => void): this {
+        return super.on(event, listener);
     }
 }
