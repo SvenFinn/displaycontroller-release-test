e92998db308a7db9fc4371d56a7131cb0160a192
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2024-10-23 17:34:53 +0200

    feat: Add source tree prep script

---
+++ src/prep.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# This script loops through all directories in the src folder and runs npm install on folders that contain a package.json file.
+
+function install_dependencies() {
+    echo "Installing dependencies in $folder"
+    cd $folder
+    rm -rf node_modules
+    rm -rf dist
+    npm install --loglevel=info
+    if [[ $folder == *"database"* ]]; then
+        npm run build
+    fi
+    cd - > /dev/null
+}
+
+function traverse_folder() {
+    for folder in $1/*; do
+        if [ -d "$folder" ]; then
+        if [[ $folder == *"node_modules"* ]] || [[ $folder == *"generated"* ]]; then
+            continue
+        fi
+        if [ -f "$folder/package.json" ]; then
+            install_dependencies $folder
+        fi
+        traverse_folder $folder
+        fi
+    done
+}
+
+traverse_folder .
