6545380b1b9abe2b178e5e66b46ba91ae4d3a77b
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2024-11-02 21:07:34 +0100

    fix: Change log message

--- src/ranges/multicast/src/updater.ts
+++ src/ranges/multicast/src/updater.ts
@@ -7,7 +7,7 @@ import { logger } from "./logger";
 const client = new LocalClient();
 
 async function update() {
-    logger.info("Updating disciplines, start list and shooters");
+    logger.info("Updating caches");
     await Promise.all([
         updateDisciplines(client),
         updateStartList(client),
