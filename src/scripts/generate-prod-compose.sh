37d85e0968f0d2a955315bf1ccfaae6ae78fbc26
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2024-12-27 15:12:19 +0100

    feat: Script for generating prod docker compose YAML

---
+++ src/scripts/generate-prod-compose.sh
@@ -0,0 +1,21 @@
+
+cd "$(dirname "$0")/.."
+
+APP_TAG=$1
+PROD_PATH=$2
+
+config=$(docker compose config)
+
+# Remove all build-related configuration
+config=$(echo "$config" | yq 'del(.services[].build)' -y )
+
+# Add tag to all images on ghcr.io
+config=$(echo "$config" | yq --arg APP_TAG "$APP_TAG" '(.services[].image | select(test("^ghcr"))) |= . + ":" + $APP_TAG' -y)
+
+# Replace all occurrences of cwd with $PROD_PATH
+# Example: turn /home/svenfinn/Dokumente/Development/displaycontroller/src/screens/images/files into /app/src/screens/images/files
+config=$(echo "$config" | yq --arg PROD_PATH "$PROD_PATH" --arg CWD "$(pwd)" '.services |= map_values(.volumes[]?.source |= gsub("^" + $CWD; $PROD_PATH))' -y)
+
+cd - > /dev/null
+
+echo "$config" > docker-compose.prod.yml
