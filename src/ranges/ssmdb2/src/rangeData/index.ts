4b13479482d4e930dcfbfb1552e492c420e8320e
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-01-08 22:41:01 +0100

    fix: Ssmdb2 losing decimals

--- src/ranges/ssmdb2/src/rangeData/index.ts
+++ src/ranges/ssmdb2/src/rangeData/index.ts
@@ -46,7 +46,7 @@ async function getHits(ssmdb2Client: Ssmdb2Client, targetId: number): Promise<Hi
             x: hit.x / 100,
             y: hit.y / 100,
             divisor: hit.dividerHundredth / 100,
-            rings: hit.rings / 10,
+            rings: hit.ringsTenth / 10,
             innerTen: hit.innerTen
         });
     }
