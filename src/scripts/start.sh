5ed331161576e5453ebe82d9b636a811973f0ced
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-01-09 21:00:51 +0100

    feat: Update start.sh

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
 
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")"
 
 PROXY_NAME="dc-reverse-proxy"
-APP_PORT=80
+APP_PORT=${APP_PORT:-80}
+
+export COMPOSE_MENU=0
+
+#screen_res=`xrandr --current | grep "*" | uniq | awk '{print $1}'`
+#echo "Screen resolution is $screen_res"
 
 # Stop the containers if they are running
 docker compose stop
 
 # Start the containers
-docker compose up&
+#SCREEN_RES=$screen_res 
+docker compose up --remove-orphans --no-build&
+docker_pid=$!
 
 # Wait for the proxy to be healthy
 docker events --filter 'event=health_status' | while read event; do
@@ -22,7 +29,27 @@ done
 unclutter -idle 0.1 -root &
 
 # Start chromium browser as kiosk 
-chromium-browser # Me needs to add parameters, but i dont have them here
+chromium-browser \
+ --kiosk \
+ --disable-pinch \
+ --overscroll-history-navigation=0 \
+ --no-first-run \
+ --disable-infobars \
+ --noerrdialogs \
+ --deny-permission-prompts \
+ --disable-audio-output \
+ --disable-breakpad \
+ --disable-crash-reporter \
+ --disable-features=TranslateUI,AutofillServerCommunication,OptimizationHintsFetching,OptimizationHint,SafeBrowsingService,SignedHTTPExchange \
+ --disable-component-update \
+ --disable-background-networking \
+ --disable-gaia-services --gaia-url=http://0.0.0.0 \
+ --disable-cache --disk-cache-dir=/dev/null --disk-cache-size=1 \
+ --enable-logging --v=1 \
+    http://localhost:$APP_PORT/show
+
+# After chromium is closed, kill unclutter and the script
+killall unclutter
 
 # Stop the containers
 docker compose stop
