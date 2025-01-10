cd30f774fe8940d4d09b4934ff4af1bf4733265e
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-01-10 17:22:39 +0100

    feat: Add recorder [skip ci]

---
+++ src/recorder/src/index.ts
@@ -0,0 +1,42 @@
+import { EventSource } from "eventsource";
+import { promises as fs } from "fs";
+import { createInterface } from "readline";
+
+const URL = "http://192.168.10.170:8080/api/ranges/sse";
+
+async function main() {
+    const handle = await fs.open("output.tmp.json", "w");
+    const es = new EventSource(URL);
+    es.onmessage = async (e) => {
+        const data = JSON.parse(e.data);
+        console.log(data);
+        data.timestamp = new Date(data.timestamp).toISOString();
+        await handle.write(JSON.stringify(data) + "\n");
+    };
+    es.onopen = () => {
+        console.log("Connected to server");
+    }
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on("line", async (line) => {
+        const text = line.trim();
+        if (text === "exit") {
+            rl.close();
+            es.close();
+            await handle.close();
+            process.exit();
+        } else {
+            const markerObj = {
+                timestamp: new Date().toISOString(),
+                marker: text
+            };
+            await handle.write(JSON.stringify(markerObj) + "\n");
+        }
+    });
+    console.log("Listening for events...");
+
+}
+
+main();
