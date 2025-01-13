e307766e7efee25fedc1eb5d777439b325f2a308
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-01-13 21:07:49 +0100

    fix: Reload logo if 50* response code

--- src/frontend/src/app/show/components/Logo/index.tsx
+++ src/frontend/src/app/show/components/Logo/index.tsx
@@ -5,7 +5,7 @@ import styles from './logo.module.css';
 
 export default function Logo(): React.JSX.Element {
     const [host, setHost] = useState<string>("");
-    const [cacheBlock, setCacheBlock] = useState<number>((new Date().getTime()));
+    const [cacheBlock, setCacheBlock] = useState<number>(Date.now());
 
     useEffect(() => {
         setHost(window.location.host.split(":")[0]);
@@ -19,11 +19,11 @@ export default function Logo(): React.JSX.Element {
 
     function onImageError(event: React.SyntheticEvent<HTMLImageElement, Event>) {
         if (event.currentTarget.naturalHeight === 0 && event.currentTarget.naturalWidth === 0) {
-            setTimeout(() => setCacheBlock((new Date()).getTime()), 1000);
+            setTimeout(() => setCacheBlock(Date.now()), 1000);
         }
     }
 
     return (
-        <img src={`http://${host}:${process.env.NEXT_PUBLIC_APP_PORT}/api/images/icon.png`} alt="Logo" className={styles.logo} onError={onImageError} />
+        <img src={logo} alt="Logo" className={styles.logo} onError={onImageError} />
     );
 }
