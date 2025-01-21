44789a960bc15ce415d3d1e8e2ef7d7630797ec8
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-01-21 22:24:41 +0100

    fix: Stop ranges from blinking

--- ranges/ssmdb2/src/rangeData/index.ts
+++ ranges/ssmdb2/src/rangeData/index.ts
@@ -1,74 +1,26 @@
-import { Ssmdb2Client } from "dc-db-ssmdb2";
 import { InternalRange } from "@shared/ranges/internal";
-import { LocalClient } from "dc-db-local";
-import { Hits } from "@shared/ranges/hits";
+import { Ssmdb2Client } from "dc-db-ssmdb2";
+import { getRangeData } from "./range";
 
-const localClient = new LocalClient();
-
-export async function getRangeData(ssmdb2Client: Ssmdb2Client, targetId: number): Promise<InternalRange | null> {
-    const data = await ssmdb2Client.target.findUnique({
-        where: {
-            id: targetId
-        }
-    });
-    if (data === null) {
-        return null;
-    }
-    const hits = await getHits(ssmdb2Client, targetId);
-    return {
-        rangeId: data.rangeId,
-        startListId: data.startListId,
-        shooter: data.shooterId,
-        hits: hits,
-        discipline: {
-            roundId: hits.length === 0 ? 0 : hits.length - 1,
-            ...await getDiscipline(data.disciplineId)
-        },
-        source: "ssmdb2",
-        ttl: 20000
-    }
-}
-
-async function getHits(ssmdb2Client: Ssmdb2Client, targetId: number): Promise<Hits> {
-    const hits = await ssmdb2Client.shot.findMany({
-        where: {
-            targetId: targetId
-        }
-    });
-    const result: Hits = [];
-    for (const hit of hits) {
-        const roundId = hit.roundId - 1;
-        if (result[roundId] === undefined) {
-            result[roundId] = [];
-        }
-        result[roundId]?.push({
-            id: hit.id,
-            x: hit.x / 100,
-            y: hit.y / 100,
-            divisor: hit.dividerHundredth / 100,
-            rings: hit.ringsTenth / 10,
-            innerTen: hit.innerTen
+export async function getRanges(ssmdb2Client: Ssmdb2Client): Promise<InternalRange[]> {
+    const localTime = new Date((new Date()).setMinutes((new Date()).getMinutes() - new Date().getTimezoneOffset()));
+    const targets = await ssmdb2Client.target.findMany(
+        {
+            where: {
+                timestamp:
+                {
+                    gt: new Date(localTime.getTime() - 1000)
+                }
+            },
+            select: {
+                rangeId: true,
+                id: true,
+                timestamp: true,
+            },
+            orderBy: {
+                timestamp: 'desc'
+            },
+            distinct: ['rangeId'],
         });
-    }
-    return result.map((round) => round?.sort((a, b) => a.id - b.id)).map((round) => round === undefined ? null : round);
-}
-
-async function getDiscipline(disciplineId: number): Promise<{ disciplineId: number } | { overrideId: number }> {
-    const isOverride = await localClient.cache.findUnique({
-        where: {
-            type_key: {
-                key: disciplineId,
-                type: "overrideDiscipline"
-            }
-        }
-    });
-    if (isOverride !== null) {
-        return {
-            overrideId: disciplineId
-        }
-    } else {
-        return {
-            disciplineId
-        }
-    }
+    return (await Promise.all(targets.map(async (target) => getRangeData(ssmdb2Client, target.id)))).filter((range) => range !== null) as InternalRange[];
 }
