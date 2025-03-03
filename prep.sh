69c45b7a328f875e9ee3c92a40a6ef2e21666bba
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-03-03 23:38:02 +0100

    fix: Build ttl

--- prep.sh
+++ prep.sh
@@ -11,7 +11,7 @@ function install_dependencies() {
     rm -rf dist
     npm install --loglevel=info
     npm update -S --loglevel=info
-    if [[ $folder == *"database"* ]] || [[ $folder == *"logger" ]]; then
+    if [[ $folder == *"database"* ]] || [[ $folder == *"logger" ]] || [[ $folder == *"ttl" ]]; then
         npm run build
     fi
     cd - > /dev/null
